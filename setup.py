b4a9299a7043482f381c61d5a1d797a458031d8f
Amol <babaiscool@gmail.com>
2015-01-30 20:21:54 +0530

    ez_setup / setuptools version

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import ez_setup
-#ez_setup.use_setuptools()
+ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 import platform
 
 from wallp.globals import Const
+
 
 entry_points = {}
 entry_points['console_scripts'] = ['wallp=wallp.main:main']
 if platform.system() == 'Windows':
 	entry_points['gui_scripts'] = ['%s=wallp.main:main'%Const.scheduler_cmd]
 
-setup(	
-	name='wallp',
+setup(	name='wallp',
 	version='1.0',
 	description='Utility to download and set desktop wallpapers from various sources.',
 	author='Amol Umrale',
@@ -22,3 +23,4 @@ setup(
 	entry_points = entry_points,
 	install_requires=['praw']
 )
+
