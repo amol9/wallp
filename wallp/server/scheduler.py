0c69dcaec337d204ff26c97c3bddfd68c7e94a7b
Amol <babaiscool@gmail.com>
2015-05-11 20:11:05 +0530

    Scheduler working; tests passing

--- wallp/server/scheduler.py
+++ wallp/server/scheduler.py
@@ -1,12 +1,15 @@
 from time import time
+import re
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor
 
+from ..globals import Const
+
 
 class Scheduler():
 	jobstores = {
-		'default' :SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+		'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
 	}
 
 	executors = {
@@ -18,25 +21,52 @@ class Scheduler():
 		'max_instances' : 1
 	}
 
+	periods = {
+		's': 'second',
+		'm': 'minute',
+		'h': 'hour',
+		'd': 'day',
+		'w': 'week',
+		'M': 'month'
+	}
+
 
 	def __init__(self):
-		self._next_run = time() + 5
-		self._apscheduler = BackgroundScheduler(jobstores=self.jobstores, executors=self.executors, job_defaults=self.job_defaults)
+		self._apscheduler = BackgroundScheduler(jobstores=self.jobstores, 
+						executors=self.executors, job_defaults=self.job_defaults)
 
 
-	def is_ready(self):
-		if time() >= self._next_run:
-			self._next_run += 100
-			return True
-		return False
+	def parse_frequency(self, freq):
+		freq_regex = re.compile("(\d{1,3})((s|m|h|d|w|M))")
+		match = freq_regex.match(freq)
+
+		if match is None:
+			raise FrequencyException('time frequency not supported')
+
+		num = match.group(1)
+		period = match.group(2)
+
+		return int(num), period
 
 
-	def add_job(self, func):
-		pass
+	def get_cron_kwarg(self, num, period):
+		return {self.periods[period]: '*/%d'%num}
 
 
-	def set_task(self, func):
-		pass
+	def add_job(self, func, freq, job_id, args=None):
+		num, period = self.parse_frequency(freq)
+		cron_kwarg = self.get_cron_kwarg(num, period)
+
+		self._apscheduler.add_job(func, 'cron', id=job_id, args=args, **cron_kwarg)
+
+
+	def remove_job(self, job_id):
+		self._apscheduler.remove_job(job_id)
+
+
+	def job_exists(self, job_id):
+		jobs = self._apscheduler.get_jobs()
+		return any([job.id == job_id for job in jobs])
 
 
 	def start(self):
@@ -47,5 +77,5 @@ class Scheduler():
 		pass
 
 
-	def stop(self):
-		pass
+	def shutdown(self):
+		self._apscheduler.shutdown()
--- wallp/test/server/test_scheduler.py
+++ wallp/test/server/test_scheduler.py
@@ -1,14 +1,94 @@
 from unittest import TestCase, main as ut_main
+from time import sleep
+import os
+from os.path import exists
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 from wallp.server.scheduler import Scheduler
 
+
+class Task():
+	def __init__ (self):
+		self.count = 0
+
+	def execute(self):
+		print('scheduled task run')
+		self.count += 1
+
+
+def task_placeholder():
+	pass
+
+
+job_db = 'test_jobs.db'
+
+
 class TestScheduler(TestCase):
 
-	def test_simple(self):
-		def task():
-			print('scheduler test task')
-			return 100
+	@classmethod
+	def setUpClass(cls):
+		cls._orig_jobstores = Scheduler.jobstores
+		Scheduler.jobstores['default'] = SQLAlchemyJobStore(url='sqlite:///' + job_db) 
 
+
+	@classmethod
+	def tearDownClass(cls):
+		Scheduler.jobstores = cls._orig_jobstores
+
+		if exists(job_db):
+			os.remove(job_db)
+
+
+	def test_simple(self):
 		sched = Scheduler()
-		#sched.set_task(task)
-		sched.add_job(
+		global task_placeholder
+
+		task = Task()
+
+		sched.add_job(task_placeholder, '3s', 'test_simple')
+		task_placeholder = task.execute
+
+		sched.start()
+		sleep(4)
+
+		self.assertEquals(1, task.count)
+
+		sched.remove_job('test_simple')
+
+		self.assertFalse(sched.job_exists('test_simple'))
+
+		sched.shutdown()
+
+
+	def test_full(self):
+		sched = Scheduler()
+		global task_placeholder
+		
+		task = Task()
+		sched.add_job(task_placeholder, '3s', 'test_full')
+		task_placeholder = task.execute
+
+		self.assertTrue(sched.job_exists('test_full'))
+
+		sched.start()
+		sleep(3.5)
+
+		self.assertEquals(1, task.count)
+
+		sleep(3.5)
+
+		self.assertEquals(2, task.count)
+
+		sched.shutdown()
+		sleep(5)
+
+		sched.start()
+		sleep(1)
+
+		self.assertEquals(2, task.count)
+
+		sched.shutdown()
+
+
+if __name__ == '__main__':
+	ut_main()
